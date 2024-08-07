4b32ca597ec37f2b0189176607663cdec0a693f0
TURBO70 <yassooff2003@gmail.com>
2024-08-08 02:04:48 +0300

    Refactor auth.routes.js and add authentication middleware

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,39 +1,44 @@
-const{
-    signup,
-    login,
-    follow,
-    unfollow,
-    search,
-    randomuser,
-    editInfo,
-    getUser,
+const {
+  signup,
+  login,
+  follow,
+  unfollow,
+  search,
+  randomuser,
+  editInfo,
+  getUser,
+} = require("../controllers/authController.controllers");
 
-}=require('../controllers/authController.controllers');
+const {
+  signupValidator,
+  loginValidator,
+} = require("../utils/validators/authValidator");
 
+const { auth } = require("../middlewares/auth");
 
-const router=require('express').Router();
-// Route to signup 
-router.post('/signup',signup);
+const router = require("express").Router();
+// Route to signup
+router.post("/signup", signupValidator, signup);
 
 // Route to login
-router.post('/login',login);
+router.post("/login", loginValidator, login);
 
 //Route to follow
-router.post('/follow',follow);
+router.post("/follow", auth, follow);
 
 //Route to unfollow
-router.post('/unfollow',unfollow);
+router.post("/unfollow", auth, unfollow);
 
 //Route to search
-router.post('/search',search);
+router.post("/search", auth, search);
 
 //Route to randomuser
-router.get('/randomuser',randomuser);
+router.get("/randomuser", auth, randomuser);
 
 //Route to editInfo
-router.post('/editInfo',editInfo);
+router.post("/editInfo", auth, editInfo);
 
 //Route to getUser
-router.get('/getUser',getUser);
+router.get("/getUser", auth, getUser);
 
-module.exports=router;
+module.exports = router;
