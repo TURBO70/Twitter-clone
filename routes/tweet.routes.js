96594164a62934c2e0105c4e88d0b8dbf5e51dda
TURBO70 <yassooff2003@gmail.com>
2024-08-07 22:43:10 +0300

    chore: Add userReplies and userLikes routes to tweet.routes.js

--- routes/tweet.routes.js
+++ routes/tweet.routes.js
@@ -9,7 +9,9 @@ const {
   getReplies,
   getTweets,
   newsfeed,
-  replyTweet
+  replyTweet,
+  userReplies,
+  userLikes,
 } = require("../controllers/tweet.controllers");
 const { auth } = require("../middlewares/auth");
 
@@ -18,5 +20,27 @@ router.post("/postTweet", auth, createTweet);
 
 // Route to get tweets
 router.get("/getTweets", auth, getTweets);
+
+// Route to like a tweet
+router.post("/likeTweet", auth, likeTweet);
+
+// Route to unlike a tweet
+router.post("/unlikeTweet", auth, unlikeTweet);
+
+// Route to get replies
+router.get("/getReplies", auth, getReplies);
+
+// Route to get newsfeed
+router.post("/newsfeed", auth, newsfeed);
+
+// Route to reply to a tweet
+router.post("/replyTweet", auth, replyTweet);
+
+// Route to get user replies 
+router.get("/userReplies", auth, userReplies);
+
+// Route to get user likes
+router.get("/userLikes", auth, userLikes);
+
 
 module.exports = router;
