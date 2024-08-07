79f260e6fefd6663fc1ae7b4adce3df955af46be
TURBO70 <yassooff2003@gmail.com>
2024-08-08 01:40:08 +0300

    chore: Add follow, unfollow, search, randomuser, editInfo, and getUser routes to auth.routes.js

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,13 +1,39 @@
 const{
     signup,
     login,
+    follow,
+    unfollow,
+    search,
+    randomuser,
+    editInfo,
+    getUser,
+
 }=require('../controllers/authController.controllers');
 
 
 const router=require('express').Router();
-
+// Route to signup 
 router.post('/signup',signup);
+
+// Route to login
 router.post('/login',login);
 
+//Route to follow
+router.post('/follow',follow);
+
+//Route to unfollow
+router.post('/unfollow',unfollow);
+
+//Route to search
+router.post('/search',search);
+
+//Route to randomuser
+router.get('/randomuser',randomuser);
+
+//Route to editInfo
+router.post('/editInfo',editInfo);
+
+//Route to getUser
+router.get('/getUser',getUser);
 
 module.exports=router;
