a01311bad5eb93c8e78dd9a9182552815d97ff5d
TURBO70 <yassooff2003@gmail.com>
2025-02-23 22:27:42 +0200

    chore: Remove commented-out code in auth middleware for clarity

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const pool = require('../config/db.config'); // Import the database pool
+const pool = require('../config/db.config'); 
 const customError = require('../utils/customError');
 
 const JWT_SECRET = process.env.SECRET_KEY;
@@ -16,14 +16,12 @@ const auth = async (req, res, next) => {
   try {
     const payload = jwt.verify(token, JWT_SECRET);
 
-    // Query the database to find the user by ID
     const result = await pool.query('SELECT * FROM users WHERE id = $1', [payload.userId]);
 
     if (result.rows.length === 0) {
       return next(new customError('Invalid Credentials', 401));
     }
 
-    // Attach the user to the request object
     req.user = result.rows[0];
     next();
   } catch (error) {
