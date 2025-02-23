8e0c250fb9ba4e9dd6948fe38dbe3e3750426d7b
TURBO70 <yassooff2003@gmail.com>
2025-02-23 22:27:57 +0200

    chore: Remove redundant comments in auth.routes.js for improved clarity

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -20,37 +20,17 @@ const {
 const { auth } = require("../middlewares/auth");
 
 const router = require("express").Router();
-// Route to signup
-router.post("/signup", signup); //
 
-// Route to login
-router.post("/login", login); //
-
-//Route to follow
-router.post("/follow", auth, follow); //
-
-//Route to unfollow
-router.post("/unfollow", auth, unfollow); //
-
-//Route to search
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/follow", auth, follow);
+router.post("/unfollow", auth, unfollow);
 router.get("/search", auth, search);
-
-//Route to randomuser
 router.get("/randomuser", auth, randomuser);
-
-//Route to editInfo
 router.post("/editInfo", auth, editInfo);
-
-//Route to getUser
-router.get("/getUser", auth, getUser); //
-
-//Route to forgotPassword
-router.post("/forgotPassword",auth, forgotPassword);
-
-//Route to verifyPassResetCode
+router.get("/getUser", auth, getUser);
+router.post("/forgotPassword", auth, forgotPassword);
 router.post("/verifyPassResetCode", verifyPassResetCode);
-
-//Route to resetPassword
 router.post("/resetPassword", resetPassword);
 
 module.exports = router;
