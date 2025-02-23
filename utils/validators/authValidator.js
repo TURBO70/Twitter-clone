33a11f2f522d37781e8159aae055ed3b9d804372
TURBO70 <yassooff2003@gmail.com>
2025-02-23 19:26:27 +0200

    chore: Update authentication validators to use PostgreSQL for email checks

--- utils/validators/authValidator.js
+++ utils/validators/authValidator.js
@@ -1,7 +1,7 @@
 const { check } = require("express-validator");
 const slugify = require("slugify");
 const validatorMiddleware = require("../../middlewares/validatorMiddleware");
-const User = require("../../models/user.models");
+const pool = require("../../config/db.config"); // Import PostgreSQL pool
 
 exports.signupValidator = [
   check("username")
@@ -13,68 +13,22 @@ exports.signupValidator = [
 
   check("email")
     .notEmpty()
-    .withMessage("email Required")
+    .withMessage("Email Required")
     .isEmail()
-    .withMessage("invalid email address")
+    .withMessage("Invalid email address")
     .toLowerCase()
-    .custom((val) =>
-      User.findOne({ email: val }).then((email) => {
-        if (email) {
-          throw new Error("E-mail already exists");
-        }
-      })
-    ),
+    .custom(async (val) => {
+      const result = await pool.query("SELECT email FROM users WHERE email = $1", [val]);
+      if (result.rows.length > 0) {
+        throw new Error("E-mail already exists");
+      }
+    }),
+
   check("password")
     .notEmpty()
     .withMessage("Password Required")
     .isLength({ min: 6 })
     .withMessage("Too short password"),
-  validatorMiddleware,
-];
 
-exports.loginValidator = [
-  check("email")
-    .notEmpty()
-    .withMessage("email reauired")
-    .isEmail()
-    .withMessage("invalid email address"),
-  check("password")
-    .notEmpty()
-    .withMessage("Password required")
-    .isLength({ min: 6 })
-    .withMessage("Too short password"),
-  validatorMiddleware,
-];
-exports.forgotPasswordValidator = [
-  check("email")
-    .notEmpty()
-    .withMessage("Email Reauired")
-    .isEmail()
-    .withMessage("Please enter a valid email address"),
-  validatorMiddleware,
-];
-
-exports.verifyPassResetCodeValidator = [
-  check("resetCode")
-    .notEmpty()
-    .withMessage("Reset code required")
-    .isLength({ min: 6 })
-    .withMessage("reset code must be 6 numbers")
-    .isLength({ max: 6 })
-    .withMessage("reset code must be 6 numbers"),
-  validatorMiddleware,
-];
-
-exports.resetPasswordValidator = [
-  check("email")
-    .notEmpty()
-    .withMessage("Email Reauired")
-    .isEmail()
-    .withMessage("Please enter a valid email address"),
-  check("newPassword")
-    .notEmpty()
-    .withMessage("new Password required")
-    .isLength({ min: 6 })
-    .withMessage("Too short password"),
   validatorMiddleware,
 ];
